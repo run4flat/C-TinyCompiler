8d40d9a99ecd25ee5e2c713ddd40631f04f5d4ed
David Mertens <dcmertens.perl@gmail.com>
2012-03-22 16:27:54 -0500

    Switched to a shared libtcc and added some docs to AV

--- lib/TCC/Perl/AV.pm
+++ lib/TCC/Perl/AV.pm
@@ -17,17 +17,139 @@ sub apply {
 	
 	# Add function declarations and symbols:
 	$state->code('Head') .= q{
+		void av_clear (AV * array);
 		int av_len (AV * av);
 		SV ** av_fetch (AV * av, int key, int lval);
 	};
 }
 
-# Retrieve the symbol pointers only once:
-my $symbols = get_symbol_ptrs();
-
 sub apply_symbols {
 	my (undef, $state) = @_;
-	$state->add_symbols(%$symbols);
+	_apply_symbols($state);
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+TCC::Perl::AV - Perl's array C-API
+
+=head1 SYNOPSIS
+
+ use TCC;
+ 
+ # Declare the compiler context with the AV bindings:
+ my $context= TCC->new(packages => '::Perl::AV');
+ 
+ # Or add them afterwards:
+ my $context = TCC->new;
+ $context->add_packages('::Perl::AV');
+ 
+ # Create a function that tells us how many arguments we sent:
+ $context->code('Body') = q{
+ 	void test_func(AV * inputs, AV * outputs) {
+ 		printf("You sent %d arguments\n", av_len(inputs));
+ 	}
+ };
+ 
+ # Compile and call:
+ $context->compile;
+ $context->call_function('test_func', 1, 2, 3);
+
+=head1 DESCRIPTION
+
+This module provides various Perl array manipulate functions to the compiler
+context. Eventually it will contain options so that you can specify which parts
+of the API you want, but for now it comes in one big bunch (or as much of it as
+I have packaged thus far).
+
+Like other TCC packages, you never use this module directly. Rather, you
+add it to your compiler context in the constructor or with the function
+L<TCC/apply_packages>.
+
+Documentation for all of these functions can be found at L<perlapi>, so I will
+only give their names and signatures here for reference (and possibly a few
+notes if I deem them to be helpful).
+
+=over
+
+=item av_clear
+
+ void av_clear (AV * array)
+
+=item av_len
+
+ int av_len (AV * array)
+
+=item av_fetch
+
+ SV ** av_fetch (AV * array, int key, int lval)
+
+Fetches the requested item from the array, creating it if necessary. The usage
+is descriped in L<perlapi>. I simply wish to point out that in my experience,
+the only time the returned pointer to the SV is only null is when I try to
+retrieve a non-existent array element B<not> in lvalue context. (I suspect that
+it may also return null in lvalue context if Perl is unable to allocate the
+contiguous memory for C<key> elements, but I have not confirmed that.) The point
+is that you B<ought to> check that the returned pointer is non-null before
+dereferencing it.
+
+=back
+
+=head1 AUTHOR
+
+David Mertens, C<< <dcmertens.perl at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-tcc at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TCC>.  I
+will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc TCC::Perl::AV
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TCC>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/TCC>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/TCC>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/TCC/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011, 2012 Northwestern University
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
--- lib/TCC/Perl/AV.xs
+++ lib/TCC/Perl/AV.xs
@@ -3,6 +3,13 @@
 #include "XSUB.h"
 
 #include "ppport.h"
+#include "libtcc.h"
+
+typedef TCCState TCCStateObj;
+
+void my_av_clear(AV * array) {
+	av_clear(array);
+}
 
 SV ** my_av_fetch(AV * array, I32 index, I32 lvalue) {
 	return av_fetch(array, index, lvalue);
@@ -14,12 +21,10 @@ int my_av_len(AV * av) {
 
 MODULE = TCC::Perl::AV           PACKAGE = TCC::Perl::AV
 
-HV *
-get_symbol_ptrs()
+void
+_apply_symbols(state)
+	TCCStateObj* state
 	CODE:
-		RETVAL = newHV();
-		/* add the function pointers */
-		hv_store(RETVAL, "av_fetch", 8, newSViv(PTR2IV(my_av_fetch)), 0);
-		hv_store(RETVAL, "av_len", 6, newSViv(PTR2IV(my_av_len)), 0);
-	OUTPUT:
-		RETVAL
+		tcc_add_symbol(state, "av_clear", my_av_clear);
+		tcc_add_symbol(state, "av_fetch", my_av_fetch);
+		tcc_add_symbol(state, "av_len", my_av_len);
