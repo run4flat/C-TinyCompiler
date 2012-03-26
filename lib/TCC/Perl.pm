8ab878c1b92c12fcf55f343fecf287951b0e389b
David Mertens <dcmertens.perl@gmail.com>
2012-03-25 21:21:48 -0500

    Added #lines to TCC::Perl and documented the error messages

--- lib/TCC/Perl.pm
+++ lib/TCC/Perl.pm
@@ -13,17 +13,23 @@ foreach (@INC) {
 }
 die "Unable to locate Perl CORE directory!" unless $core_dir;
 
+# Pull in the local includes:
+use Config;
+my @local_inc = grep {-d} split / /, $Config{locincpth};
+#print "Local includes are ", join('|', @local_inc), "\n";
+
 sub apply {
 	my (undef, $state) = @_;
 	
 	# Add Perl's CORE directory to the compiler's list of includes:
-	$state->add_include_path($core_dir);
+	$state->add_include_path($_) foreach ($core_dir, @local_inc);
 	
 	# Add function declarations and symbols:
-	$state->code('Head') .= q{
+	$state->code('Head') .= TCC::line_number(__LINE__) . q{
 		#include "EXTERN.h"
 		#include "perl.h"
 		#include "XSUB.h"
+		#line 1 "whatever comes after TCC::Perl"
 	};
 }
 
@@ -77,6 +83,32 @@ to be quite a blunt tool.
 (What follows is what I *hope* happens, though it is not yet the reality.) This
 great weight is the reason the TCC::Perl::* sub-modules exist. They pull in and
 define only the exact code that you need.
+
+=head1 ERROR MESSAGES
+
+This module may lead to some interesting error messages. Here's a reference.
+
+=over
+
+=item Unable to locate Perl CORE directory!
+
+This compile-time error will indicate that TCC::Perl was not able to find the
+CORE directory in your Perl libs. This is a big issue because the header files
+that TCC::Perl wants to include in your compiler context can only be found in
+this directory. If you run into this problem, it means you do not have your
+Perl header files stored in a customary place. If you don't know how to fix
+this, you should reach out to me or to the Perl community (i.e. PerlMonks) for
+help.
+
+=item whatever comes after TCC::Perl
+
+If you see anything with this text in it, you have encountered an error
+(probably a compile error) in code that you added to your compiler's Head
+section after you added the TCC::Perl package. Check any and all code that you
+added. Using L<TCC/line_numbers> may help narrow-down the errant line in your
+code.
+
+=back
 
 =head1 AUTHOR
 
