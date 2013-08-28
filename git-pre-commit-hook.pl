1b484effd2e65eed0e7037159993ec8821e3a765
David Mertens <dcmertens.perl@gmail.com>
2013-08-28 08:09:17 -0400

    Fixed git commit hook

--- git-pre-commit-hook.pl
+++ git-pre-commit-hook.pl
@@ -5,17 +5,20 @@ use warnings;
 # Stash the unstaged modifications:
 `git stash save --keep-index`;
 
+# Assume good unless we find a problem
 my $to_return = 0;
 
 # Did I remember to touch the Changes file?
 my $touched_files = `git status --porcelain`;
 if ($touched_files !~ /Changes/) {
+	# Assume bad unless the user overrides
+	$to_return = 1;
 	$|++;
 	print "It looks like you didn't update the Changes file. Should I continue (y/n)? ";
 	open STDIN, '<', '/dev/tty';
 	my $response = <>;
 	
-	$to_return = 1 if $response eq 'y';
+	$to_return = 0 if $response =~ 'y';
 }
 
 # Restore the stash and return the result
